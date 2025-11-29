2492df6cba8abc146790615c13e4186c85237f65
brettkues <brett.kues@gmail.com>
2025-11-29 15:11:53 -0600

    Create route.js

---
+++ app/api/scrape/route.js
@@ -0,0 +1,35 @@
+import * as cheerio from "cheerio";
+
+export async function POST(request) {
+  try {
+    const { url } = await request.json();
+
+    if (!url) {
+      return new Response(JSON.stringify({ error: "No URL provided" }), {
+        status: 400,
+      });
+    }
+
+    // Fetch the HTML
+    const res = await fetch(url);
+    const html = await res.text();
+
+    const $ = cheerio.load(html);
+
+    // Find all image URLs
+    const images = [];
+
+    $("img").each((i, el) => {
+      const src = $(el).attr("src");
+      if (src && images.length < 20) {
+        images.push(src);
+      }
+    });
+
+    return new Response(JSON.stringify({ images }), { status: 200 });
+  } catch (err) {
+    return new Response(JSON.stringify({ error: err.message }), {
+      status: 500,
+    });
+  }
+}
