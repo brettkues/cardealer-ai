06f5ac39aec4d432555b1496fcdffe0cbbb0bcde
brettkues <brett.kues@gmail.com>
2025-11-29 14:44:08 -0600

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,9 @@
+export const metadata = { title: "CarDealer-AI", description: "Clean build" };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
