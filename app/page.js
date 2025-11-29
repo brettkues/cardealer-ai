21e5af11023f9d431aa28ec316e257ecdd3c09aa
brettkues <brett.kues@gmail.com>
2025-11-29 14:55:51 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div style={{padding: 40, fontSize: 24}}>
+    <div style={{ padding: 40, fontSize: 24 }}>
       Clean Next.js + Tailwind starter deployed successfully.
     </div>
   );
