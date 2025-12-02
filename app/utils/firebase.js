8e6948f2a7c71c023194de327e6049af4e0d882f
brettkues <brett.kues@gmail.com>
2025-12-02 06:34:18 -0600

    Update firebase.js

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -1,6 +1,7 @@
 // app/utils/firebase.js
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB2B08TUUYJwLTGiT1dTlowc64-aJAXjjE",
@@ -11,6 +12,11 @@ const firebaseConfig = {
   appId: "1:886575208060:web:23e5ff2001e9a518e09bf9",
 };
 
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const app =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
+// Firestore
 export const db = getFirestore(app);
+
+// Firebase Storage (required)
+export const storage = getStorage(app);
