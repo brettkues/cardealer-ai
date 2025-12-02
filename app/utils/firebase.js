3c43d6823deeb9b0787fce48212517eb79116b12
brettkues <brett.kues@gmail.com>
2025-12-01 19:45:13 -0600

    Create firebase.js

---
+++ app/utils/firebase.js
@@ -0,0 +1,16 @@
+// app/utils/firebase.js
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2B08TUUYJwLTGiT1dTlowc64-aJAXjjE",
+  authDomain: "cardealer-ai.firebaseapp.com",
+  projectId: "cardealer-ai",
+  storageBucket: "cardealer-ai.firebasestorage.app",
+  messagingSenderId: "886575208060",
+  appId: "1:886575208060:web:23e5ff2001e9a518e09bf9",
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export const db = getFirestore(app);
