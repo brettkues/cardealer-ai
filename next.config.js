33deeb0fa4ee8ea323eabe7041ffbfac91c987df
brettkues <brett.kues@gmail.com>
2025-12-04 05:55:49 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-
-  // Needed for your API routes (scraper, collage, chat, stripe)
-  api: {
-    responseLimit: "20mb",
-    bodyParser: {
-      sizeLimit: "15mb",
-    },
-  },
-
-  // Next 14: remove deprecated serverActions
   experimental: {
-    serverMinify: false,
-  },
-
-  // Prevent Vercel build errors with Node polyfills
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      path: false,
-    };
-    return config;
-  },
+    serverActions: true,
+    webpackBuildWorker: true
+  }
 };
 
 module.exports = nextConfig;
