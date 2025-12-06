0bb46c8871887608245833d821b48270abafc000
brettkues <brett.kues@gmail.com>
2025-12-06 07:15:08 -0600

    Delete app directory

--- app/api/session/get/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const response = NextResponse.json({ status: "ok" });
-
-  const uid = response.cookies.get("uid")?.value || null;
-
-  return NextResponse.json({ uid });
-}
