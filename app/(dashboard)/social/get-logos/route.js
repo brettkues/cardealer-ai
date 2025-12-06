ae1b6d0148c90050ea4e00150b60d7fd8a5bde0e
brettkues <brett.kues@gmail.com>
2025-12-06 11:38:43 -0600

    Create route.js

---
+++ app/(dashboard)/social/get-logos/route.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { adminStorage } from "../../../../../lib/firebaseAdmin";
+
+export async function GET() {
+  try {
+    const bucket = adminStorage.bucket();
+    const [files] = await bucket.getFiles({ prefix: "logos/" });
+
+    const urls = [];
+
+    for (const file of files) {
+      const [url] = await file.getSignedUrl({
+        action: "read",
+        expires: "03-01-2035",
+      });
+
+      urls.push(url);
+    }
+
+    return NextResponse.json({ logos: urls });
+  } catch (err) {
+    return NextResponse.json({ logos: [] });
+  }
+}
