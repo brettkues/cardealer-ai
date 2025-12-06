1da39c4ee737fa194dd53746d4c065cc18251ce9
brettkues <brett.kues@gmail.com>
2025-12-06 07:26:24 -0600

    Create page.js

---
+++ app/(dashboard)/page.js
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold mb-4">Welcome</h1>
+      <p>Select a tool from the sidebar to get started.</p>
+    </div>
+  );
+}
