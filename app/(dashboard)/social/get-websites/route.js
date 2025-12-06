fd7b237d2b14b87ff6308807301a3139e8ba9142
brettkues <brett.kues@gmail.com>
2025-12-06 11:34:41 -0600

    Create route.js

---
+++ app/(dashboard)/social/get-websites/route.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { adminDB } from "../../../../../lib/firebaseAdmin";
+
+export async function GET() {
+  try {
+    const snapshot = await adminDB.collection("websites").get();
+    const list = snapshot.docs.map((d) => d.data().url);
+
+    return NextResponse.json({ websites: list });
+  } catch (err) {
+    return NextResponse.json({ websites: [] });
+  }
+}
