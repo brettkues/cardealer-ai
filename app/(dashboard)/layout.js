0f2dcb4f5759fed2193363c8316a2e04adf3ddb7
brettkues <brett.kues@gmail.com>
2025-12-06 07:25:20 -0600

    Create layout.js

---
+++ app/(dashboard)/layout.js
@@ -0,0 +1,17 @@
+import "../globals.css";
+import Sidebar from "../../components/Sidebar";
+
+export const metadata = {
+  title: "Dashboard",
+};
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div className="flex min-h-screen bg-gray-100 text-black">
+      <Sidebar />
+      <main className="flex-1 p-6">
+        {children}
+      </main>
+    </div>
+  );
+}
