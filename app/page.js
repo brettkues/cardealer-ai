5a095a82c4d0bbc896c45748a15325ce0b3bf3da
brettkues <brett.kues@gmail.com>
2025-12-06 18:56:16 -0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold">Welcome</h1>
+      <p>Please log in or sign up to continue.</p>
+    </div>
+  );
+}
