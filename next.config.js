36d7334008b827f7ca66983f396fa2515edb58be
brettkues <brett.kues@gmail.com>
2025-12-08 11:42:20 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-
   experimental: {
     serverActions: true,
   },
-
-  api: {
-    responseLimit: "20mb",
-    bodyParser: {
-      sizeLimit: "15mb",
-    },
-  },
-
-  webpack: (config) => {
-    // Disable Node modules not available in Vercel Edge
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-    };
-
-    return config;
+  async rewrites() {
+    return [
+      {
+        source: "/__/auth/:path*",
+        destination: "/__/auth/:path*",
+      },
+    ];
   },
 };
 
