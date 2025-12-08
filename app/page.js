6f3fc1ff449e77f4c488465f02f83d6b52adeef2
brettkues <brett.kues@gmail.com>
2025-12-07 18:44:21 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  redirect("/login");
-  return null; // fallback for React (never actually reached)
+  redirect("/auth/login");
+  return null;
 }
