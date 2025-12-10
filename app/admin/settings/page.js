27d9a68c0bfc3b606d02a55f20944a72198c10da
brettkues <brett.kues@gmail.com>
2025-12-09 20:59:03 -0600

    Create page.js

---
+++ app/admin/settings/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function AdminSettings() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Global Settings</h1>
+
+      <p className="text-gray-600">
+        Placeholder page. Add global configuration here.
+      </p>
+    </div>
+  );
+}
