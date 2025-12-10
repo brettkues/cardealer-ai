0887be54da8836417d16691d000303e774ab3a49
brettkues <brett.kues@gmail.com>
2025-12-09 20:59:27 -0600

    Create page.js

---
+++ app/admin/tools/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function AdminTools() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Tool Settings</h1>
+
+      <p className="text-gray-600">
+        Placeholder page. Configure collage generator & scraper here.
+      </p>
+    </div>
+  );
+}
