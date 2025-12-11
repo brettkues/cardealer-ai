06258c91ab1958729e646d895254d84a59142b4d
brettkues <brett.kues@gmail.com>
2025-12-11 13:31:31 -0600

    Create page.js

---
+++ app/image-generator/websites/page.js
@@ -0,0 +1,8 @@
+export default function IGWebsitesPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Websites</h1>
+      <p className="mt-4">Website management will appear here.</p>
+    </div>
+  );
+}
