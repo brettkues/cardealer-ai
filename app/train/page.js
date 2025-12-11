80d318949429dd3d483ac1269da1c3bb4dc17bfd
brettkues <brett.kues@gmail.com>
2025-12-11 13:30:27 -0600

    Create page.js

---
+++ app/train/page.js
@@ -0,0 +1,8 @@
+export default function TrainAIPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Train Your AI</h1>
+      <p className="mt-4">Sales & F&I training tools will appear here.</p>
+    </div>
+  );
+}
