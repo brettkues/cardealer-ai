cb6da44478457dbdfa5fb55193870c283a78d478
brettkues <brett.kues@gmail.com>
2025-12-11 11:34:36 -0600

    Delete app/page.js

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>Welcome to the AI Dealership Platform</h1>
-      <p>Please <a href="/login">log in</a> to access your tools.</p>
-    </div>
-  );
-}
