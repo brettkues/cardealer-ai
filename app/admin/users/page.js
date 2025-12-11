136ddc3f1a6842aa7e4b6d84827db53f858fac9f
brettkues <brett.kues@gmail.com>
2025-12-11 13:29:27 -0600

    Create page.js

---
+++ app/admin/users/page.js
@@ -0,0 +1,8 @@
+export default function AdminUsersPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">User Management</h1>
+      <p className="mt-4">User tools will appear here.</p>
+    </div>
+  );
+}
