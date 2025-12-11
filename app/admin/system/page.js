d05f439e96e706c56520484be1a4c9eee606c06d
brettkues <brett.kues@gmail.com>
2025-12-11 13:29:46 -0600

    Create page.js

---
+++ app/admin/system/page.js
@@ -0,0 +1,8 @@
+export default function AdminSystemPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">System Activity</h1>
+      <p className="mt-4">System logs will appear here.</p>
+    </div>
+  );
+}
