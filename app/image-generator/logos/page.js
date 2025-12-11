93f46f2fe3ea22b40d36c614d419e471f1814d6c
brettkues <brett.kues@gmail.com>
2025-12-11 13:31:14 -0600

    Create page.js

---
+++ app/image-generator/logos/page.js
@@ -0,0 +1,8 @@
+export default function IGLogosPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Logos</h1>
+      <p className="mt-4">Logo upload & management will appear here.</p>
+    </div>
+  );
+}
