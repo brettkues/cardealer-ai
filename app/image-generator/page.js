b38133d27124cf81abd72537956ed2dd6d910fd4
brettkues <brett.kues@gmail.com>
2025-12-11 13:30:53 -0600

    Create page.js

---
+++ app/image-generator/page.js
@@ -0,0 +1,8 @@
+export default function ImageGeneratorPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Social Image Generator</h1>
+      <p className="mt-4">Tools for building social images will appear here.</p>
+    </div>
+  );
+}
