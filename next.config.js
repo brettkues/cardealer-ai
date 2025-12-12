581a0d46aacdc5349578c9a7574ceb3b8a4b786b
brettkues <brett.kues@gmail.com>
2025-12-12 12:02:16 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-
-  // Disable all tracing so Vercel cannot recurse into node_modules
   experimental: {
-    serverActions: true,
-    serverComponentsExternalPackages: ["sharp"],
-    outputFileTracingRoot: undefined,
-    outputFileTracing: false,
-    turbo: false,
-  },
-
-  // Completely stop Next.js from analyzing dependencies
-  output: "standalone",
-
-  // Override Webpack to prevent micromatch recursion
-  webpack(config) {
-    config.externals = [...config.externals, "micromatch"];
-    config.snapshot = { managedPaths: [], immutablePaths: [] };
-    return config;
-  },
-
-  api: {
-    responseLimit: "20mb",
-    bodyParser: {
-      sizeLimit: "15mb",
-    },
-  },
+    serverComponentsExternalPackages: ["@napi-rs/canvas"]
+  }
 };
 
 module.exports = nextConfig;
