e17db0392e131f7338016a2f5d06f82bc6249058
brettkues <brett.kues@gmail.com>
2025-12-12 12:39:24 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-
   experimental: {
-    serverActions: true,
-    serverComponentsExternalPackages: ["@napi-rs/canvas"],
-  },
-
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": require("path").resolve(__dirname),
-    };
-
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-    };
-
-    return config;
-  },
+    serverComponentsExternalPackages: ["@napi-rs/canvas"]
+  }
 };
 
 module.exports = nextConfig;
