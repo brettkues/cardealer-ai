bf04f0f26422e14cf47822b64d0741ca8fb73253
brettkues <brett.kues@gmail.com>
2025-12-12 15:12:15 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-
-  experimental: {
-    serverActions: true,
-    serverComponentsExternalPackages: ["sharp"]    // REQUIRED FOR VERCEL
-  },
-
-  api: {
-    responseLimit: "20mb",
-    bodyParser: {
-      sizeLimit: "15mb"
-    }
-  },
-
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false
-    };
-
-    return config;
-  }
+  reactStrictMode: false
 };
 
 module.exports = nextConfig;
