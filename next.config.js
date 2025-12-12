beb58c895b0029c2991c6d6fc33bfb2fcd0c44c6
brettkues <brett.kues@gmail.com>
2025-12-12 15:13:59 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+
+  // Required for server-side image generation using Sharp
+  experimental: {
+    serverComponentsExternalPackages: ["sharp"]
+  },
+
+  // Do NOT include custom webpack fallbacks here.
+  // Do NOT include custom "api" config blocks.
+  // Do NOT include path fallbacks.
 };
 
 module.exports = nextConfig;
