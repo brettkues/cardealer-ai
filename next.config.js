01a89382f42eefb992dbd7d272c0a6fe993d76df
brettkues <brett.kues@gmail.com>
2025-12-13 13:00:07 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-
-  webpack(config, { isServer }) {
-    // DEBUG: Print the directories Next.js is scanning
-    console.log("WEBPACK CONTEXT:", config.context);
-    console.log("RESOLVE MODULES:", config.resolve.modules);
-
-    return config;
-  },
-
-  experimental: {
-    serverActions: true,
-    // DEBUG: This forces Next.js to spit out patterns before crashing
-    outputFileTracingIncludes: {
-      "/": ["./**/*"]
-    }
-  }
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
